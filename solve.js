ce41e32b70b56e8e1094a3cd5166c07fc9de1b07
guodp <guodp9u0@gmail.com>
2020-05-09 10:24:22 +0800

    update

--- solve.js
+++ solve.js
@@ -1,8 +1,11 @@
-const moment = require('C:/Data/node_global/node_modules/moment')
+const moment = require('C:/Data/node_global/node_modules/moment');
 
-
+// 包辰的生日是一年中的第几天
 const daysOfBC = moment('19920819').dayOfYear();
+// 郭登鹏的生日是一年中的第几天
 const daysOfGDP = moment('19910305').dayOfYear();
-const days = (daysOfBC - daysOfGDP) * 0.618 + daysOfGDP;
-const date = moment('20200101').add(~~days, 'day');
+// 黄金分割点
+const daysOfYear = (daysOfBC - daysOfGDP) * 0.618 + daysOfGDP;
+// 第一天 + 向下取整
+const date = moment('20200101').add(~~daysOfYear, 'day');
 console.log(date.format('YYYYMMDD'));
